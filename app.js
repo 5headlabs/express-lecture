3622ffe22a8c3ef9c4565703bcdf7f31581f123f
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2022-05-07 14:45:15 +0200

    [ADDED] view engine to app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 //import router
 const router = require('./routes/fruits');
 
+//set ejs as view engine
+app.set('view engine', 'ejs');
+
 //direct request handling
 app.get('/', function(req, res) {
   res.redirect("/fruits");
