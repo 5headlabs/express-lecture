cad38119953c5d698f9dc310e1c7a86fd32e4b18
MalteJosten <malte.josten@stud.uni-due.de>
2022-05-09 08:03:25 +0200

    [MODIFIED] commenting

--- controllers/fruitController.js
+++ controllers/fruitController.js
@@ -9,17 +9,17 @@ exports.listFruits = (req, res) => {
     });
 }
 
-// controller handler function for fruit details
+// Controller handler function for fruit details
 exports.getFruitDetails = function(req, res) {
-    // get the fruit id from the URL parameter list
+    // Get the fruit id from the URL parameter list
     let fruitID = req.params.fruitID;
-    // get fruit object
+    // Get fruit object
     Fruit.findById(fruitID,
         (err, result) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log(result);
+                //console.log(result);
                 res.render('fruit', {
                     fruit: result,
                     back_url: "/fruits"
@@ -28,7 +28,7 @@ exports.getFruitDetails = function(req, res) {
         });
 }
 
-// controller handler function for creating a fruit
+// Controller handler function for creating a fruit
 exports.createFruit = function(req, res) {
     console.log(req.body);
     let fruit = new Fruit({
