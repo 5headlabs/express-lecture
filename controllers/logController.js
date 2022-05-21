3d859641c9c12006671bbc2db1f98e7e6e1af35c
MalteJosten <malte.josten@stud.uni-due.de>
2022-05-21 09:15:11 +0200

    [FIXED] typo

--- controllers/logController.js
+++ controllers/logController.js
@@ -2,6 +2,6 @@ exports.logActivity = (req, res, next) => {
     // Print activity log to console
     console.log(`Retrieving info for item '${req.params.fruitID}'`);
 
-    // Call next function in middleware stack
+    // Call next function in middleware chain
     next();
 }
